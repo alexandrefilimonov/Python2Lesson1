222e3b75b55c5f6d295485e265418ef3d8687b8a
alexandrefilimonov <alexflido@yahoo.com>
2018-11-05 01:08:46 -0800

    Python2 Lesson1

---
+++ _alexfilimonov_python2_lesson1_method1.py
@@ -0,0 +1,18 @@
+﻿#1. Каждое из слов «разработка», «сокет», «декоратор» представить в строковом формате 
+#и проверить тип и содержание соответствующих переменных. Затем с помощью онлайн-конвертера преобразовать 
+#строковые представление в формат Unicode и также проверить тип и содержимое переменных.
+
+a= 'разработка'
+b= 'сокет'
+c= 'декоратор'
+print('Variable a has type - ',type(a),', content - ',a,' length - ', len(a)) 
+print('Variable b has type - ',type(b),', content - ',b,' length - ', len(b)) 
+print('Variable c has type - ',type(c),', content - ',c,' length - ', len(c)) 
+
+a= 'разработка'.encode('Utf-8')
+b= 'сокет'.encode('Utf-8')
+c= 'декоратор'.encode('Utf-8')
+
+print('Variable a has type - ',type(a),', content - ',a,' length - ', len(a)) 
+print('Variable b has type - ',type(b),', content - ',b,' length - ', len(b)) 
+print('Variable c has type - ',type(c),', content - ',c,' length - ', len(c)) 
---
+++ _alexfilimonov_python2_lesson1_method3.py
@@ -0,0 +1,8 @@
+﻿#3. Определить, какие из слов «attribute», «класс», «функция», «type» невозможно записать в байтовом типе.
+
+b = b'attribute'
+#b = b'класс'
+#b = b'функция'
+b = b'type'
+
+print('Bytes can only contain ASCII literal character, because of that «attribute» and «type» can be in 1 byte per character format, the words in cyrillic are not!') 
